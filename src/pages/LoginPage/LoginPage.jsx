9006cabeea44efb040c5ff685acc65354e477263
alexdevsprint <alexdevsprint@gmail.com>
2025-06-05 14:32:19 +0300

    commit

---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,10 @@
+import LoginForm from "../../components/LoginFrom/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Login Form</h1>
+      <LoginForm />
+    </div>
+  );
+}
---
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -0,0 +1,10 @@
+import RegistrationForm from "../../components/RegistrationForm/RegistrationForm";
+
+export default function RegistrationPage() {
+  return (
+    <div>
+      <h1>Registration Form</h1>
+      <RegistrationForm />
+    </div>
+  );
+}
