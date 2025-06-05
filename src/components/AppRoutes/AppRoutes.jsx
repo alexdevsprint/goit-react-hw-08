5a6f0bec70bf12c69f874d1ba945eef82486691d
alexdevsprint <alexdevsprint@gmail.com>
2025-06-05 14:54:17 +0300

    commit

---
+++ src/components/AppRoutes/AppRoutes.jsx
@@ -0,0 +1,20 @@
+import { lazy, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+
+const HomePage = lazy(() => import("../../pages/HomePage/HomePage"));
+const RegistrationPage = lazy(() => import("../../pages/RegistrationPage/RegistrationPage"));
+const LoginPage = lazy(() => import("../../pages/LoginPage/LoginPage"));
+const ContactsPage = lazy(() => import("../../pages/ContactsPage/ContactsPage"));
+
+export default function AppRoutes() {
+  return (
+    <Suspense fallback={<p>Loading page...</p>}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegistrationPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/contacts" element={<ContactsPage />} />
+      </Routes>
+    </Suspense>
+  );
+}
