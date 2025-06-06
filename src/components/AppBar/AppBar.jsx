fba9f9fd9fe873ce4f00507e5a5d10dc68cb55b8
alexdevsprint <alexdevsprint@gmail.com>
2025-06-06 12:11:59 +0300

    commit

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,13 +1,19 @@
+import css from "./AppBar.module.css";
 import AppNavigation from "../AppNavigation/AppNavigation";
 import UserMenu from "../UserMenu/UserMenu";
 import AuthNav from "../AuthNav/AuthNav";
 
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
+
 export default function AppBar() {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
   return (
-    <header>
+    <header className={css.header}>
       <AppNavigation />
-      <UserMenu />
-      <AuthNav />
+      {isLoggedIn && <UserMenu />}
+      {!isLoggedIn && <AuthNav />}
+      
     </header>
   );
 }
--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,10 +1,16 @@
+import css from "./AuthNav.module.css";
 import { NavLink } from "react-router-dom";
+import clsx from 'clsx';
+
+const getActiveLink = ({ isActive }) => {
+  return clsx(css.link, isActive && css.isActive);
+};
 
 export default function AuthNav() {
   return (
-    <nav>
-      <NavLink to="/register">Sign Up</NavLink>
-      <NavLink to="/login">Login</NavLink>
+    <nav className={css.nav}>
+      <NavLink to="/register" className={getActiveLink}>Sign Up</NavLink>
+      <NavLink to="/login" className={getActiveLink}>Login</NavLink>
     </nav>
   );
 }
