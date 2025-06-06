fb74b78f614c62500190ddb026746bbe28cbdb4f
alexdevsprint <alexdevsprint@gmail.com>
2025-06-06 17:46:55 +0300

    commit

--- src/components/AppNavigation/AppNavigation.jsx
+++ src/components/AppNavigation/AppNavigation.jsx
@@ -1,16 +1,25 @@
 import { NavLink } from "react-router-dom";
-import css from "./AppNavigation.module.css"
-import clsx from 'clsx';
+import css from "./AppNavigation.module.css";
+import clsx from "clsx";
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
 
 const getActiveLink = ({ isActive }) => {
   return clsx(css.link, isActive && css.isActive);
 };
 
 export default function AppNavigation() {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
   return (
     <nav className={css.nav}>
-      <NavLink to="/" className={getActiveLink}>Home</NavLink>
-      <NavLink to="/contacts" className={getActiveLink}>Contacts</NavLink>                
+      <NavLink to="/" className={getActiveLink}>
+        Home
+      </NavLink>
+      {isLoggedIn && (
+        <NavLink to="/contacts" className={getActiveLink}>
+          Contacts
+        </NavLink>
+      )}
     </nav>
   );
 }
