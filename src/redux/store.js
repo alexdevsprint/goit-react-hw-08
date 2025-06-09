1a678a84518d6382f4053ec40717a108b5a14129
alexdevsprint <alexdevsprint@gmail.com>
2025-06-09 11:17:09 +0300

    commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,40 @@
 import { configureStore } from "@reduxjs/toolkit";
 import contactsReducer from "./contacts/slice";
 import filterReducer from "./filtersSlice";
-import authReducer from "./auth/slice"
+import authReducer from "./auth/slice";
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "user-token",
+  storage,
+  whitelist: ["token"],
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
     filters: filterReducer,
-    auth: authReducer,
+    auth: persistedAuthReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {        
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
